c939136d3cd5b99dad7bfa89cf959c512c2393cb
Sergey Korsensky <sergey.korsensky@intel.com>
2024-12-15 17:15:49 +0200

    Implement Set-EnvVar.ps1:
    Tests do not work.

---
+++ Set-EnvVar.ps1
@@ -0,0 +1,71 @@
+<#
+.SYNOPSIS
+Sets a machine-level environment variable.
+
+.DESCRIPTION
+The Set-EnvVar function sets a machine-level environment variable with the specified name and value.
+If the environment variable does not already exist, it will be created.
+
+.PARAMETER Name
+The name of the environment variable to set.
+
+.PARAMETER Value
+The value of the environment variable to set.
+
+.EXAMPLE
+Set-EnvVar -Name "MyVariable" -Value "MyValue"
+This command sets the machine-level environment variable "MyVariable" to "MyValue".
+
+.EXAMPLE
+Set-EnvVar -Name "Path" -Value "C:\MyPath"
+This command sets the machine-level environment variable "Path" to "C:\MyPath".
+
+.NOTES
+Author: Your Name
+Date: Today's Date
+#>
+
+class _CEnvironmentWin
+{
+    static [string] GetEnvironmentVariable([string]$Name)
+    {
+        return [System.Environment]::GetEnvironmentVariable($Name, [System.EnvironmentVariableTarget]::Machine);
+    }
+    static [string] SetEnvironmentVariable([string]$Name, [string]$Value)
+    {
+        return [System.Environment]::SetEnvironmentVariable($Name, $Value, [System.EnvironmentVariableTarget]::Machine);
+    }
+}
+
+[object]$EnvironmentClass=[_CEnvironmentWin]
+
+function Set-EnvVar {
+    param (
+        [string]$Name,
+        [string]$Value
+    )
+    $CurrValue = $EnvironmentClass::GetEnvironmentVariable($Name)
+
+    if ($CurrValue -ne $Value) {
+        if ($null -eq $CurrValue) {
+            Write-Message -message "Setting environment variable $Name to $Value"
+        }
+        else {
+            Write-Message -message "Updating environment variable $Name from $CurrValue to $Value"
+        }
+
+        try {
+            $EnvironmentClass::SetEnvironmentVariable($Name, $Value)
+            Write-Message -message "Setting environment variable $Name to $Value succeeded"
+        }
+        catch {
+            Write-Message -message "Failed to set environment variable $Name to $Value. Exception: $_"
+            Write-Message -message "Please make sure you are running as an administrator"
+            throw
+        }
+    } 
+    else {
+        Write-Message -Message "Environment variable $Name is already set to $Value"
+    }
+}
+
---
+++ Tests/Set-EnvVar.Tests.ps1
@@ -0,0 +1,62 @@
+# Import the script to be tested
+BeforeAll {
+    . "$PSScriptRoot/../Set-EnvVar.ps1"
+    . "$PSScriptRoot/../Write-Message.ps1"
+}
+
+
+Describe "Set-EnvVar Function Tests" {
+    BeforeAll {
+        Mock Write-Message
+    }
+
+    Context "When setting a new environment variable" {
+        It "Should create a new machine-level environment variable" {
+            # Arrange
+
+            class _CEnvironmentStub
+            {
+                static [string] $_envVarValue = $null;
+            
+                static [string] GetEnvironmentVariable([string]$Name)
+                {
+                    return [_CEnvironmentStub]::_envVarValue;
+                }
+                static [string] SetEnvironmentVariable([string]$Name, [string]$Value)
+                {
+                    return [_CEnvironmentStub]::_envVarValue = $Value;
+                }
+            }
+            
+            $envName = "TestEnvVar"
+            $envName = "TestValue"
+            $EnvironmentClass = [_CEnvironmentStub]
+            $EnvironmentClass::SetEnvironmentVariable($envName, $null)
+            # Act
+            Set-EnvVar -Name $envName -Value $envValue
+
+            # Assert
+            Should -Invoke Write-Message #-ParameterFilter  { $message -eq "Setting environment variable $envName to $envName" }
+        }
+    }
+
+#    Context "When updating an existing environment variable" {
+#        It "Should update the machine-level environment variable" {
+#            # Arrange
+#            $envName = "TestEnvVar"
+#            $initialValue = "InitialValue"
+#            $updatedValue = "UpdatedValue"
+#            Mock -CommandName '[System.Environment]::GetEnvironmentVariable' -MockWith { return $initialValue }
+#
+#            # Act
+#            Set-EnvVar -Name $envName -Value $updatedValue
+#
+#            # Assert
+#            Should -Invoke [System.Environment]::SetEnvironmentVariable -Times 1 -Exactly -ParameterFilter {
+#                $args[0] -eq $envName -and
+#                $args[1] -eq $updatedValue -and
+#                $args[2] -eq [System.EnvironmentVariableTarget]::Machine
+#            }
+#        }
+#    }
+}
